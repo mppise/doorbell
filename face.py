7c3c3e717b003c2975a2bf931dce8ccf90181381
mppise <mppise@gmail.com>
2017-01-23 01:22:00 -0500

    working

--- face.py
+++ face.py
@@ -1,27 +1,35 @@
 import numpy as np
-import cv2
-from PIL import Image
-import time
-import os
+import cv2,time,os
 
 # -- Configurations -- #
-HAR_CLAS = "haarcascade_frontalface_alt.xml"
+HAR_CLAS = "haarcascade_frontalface_default.xml"
 IMG_SIZE = 95
-IMG_QUAL = 150
+IMG_QUAL = 75
 
 face_cascade = cv2.CascadeClassifier(HAR_CLAS)
 cam = cv2.VideoCapture(0)
-cam.set(3, 1280)    # Width
-cam.set(4, 960)     # Height
-cam.set(5, 30)      # FPS
+cam.set(3, 960) # Width
+cam.set(4, 720) # Height
 
 facenum = 0
+working = 0
 while(True):
     ret, img = cam.read()
     if(ret == False):
         print("Error: Camera is possibly disconnected or busy")
         break
     else:
+        if(working > 0):
+            cv2.imshow("Main Door", img)
+            if(cv2.waitKey(250) & 0xFF == ord('q')):
+                working = 0
+                print("Face tracking resumed")
+                continue
+            working = working -1
+            if(working % 50 == 0):
+                print("Face tracking is paused ...")
+            continue
+        
         imgquality = int(cv2.Laplacian(img, cv2.CV_64F).var())
 
         #- Get Faces in the crowd, if any
@@ -44,8 +52,12 @@ while(True):
                         cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,255), 2)
                     else:
                         imgcrop = img[y-(h*0.5):(y+h+(h*0.5)), x-(w*0.5):(x+w+(w*0.5))]
-                        cv2.imwrite('/home/pi/apps/clicks/face_'+str(facenum)+'.jpg', imgcrop)
+                        #- Save sharpened image
+                        cv2.imwrite('/home/pi/apps/clicks/face_'+str(facenum)+'.jpg', cv2.filter2D( imgcrop, -1, np.array([[0,0,0], [0,1,0], [0,0,0]]) ))
+                        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0), 4)
                         facenum = facenum+1
+            cv2.imshow("Main Door", img)
+            cv2.waitKey(5000)
         
             if(len(faces) == facenum):
                 print("Got all faces. Uploading for identification...")
@@ -55,17 +67,8 @@ while(True):
                     file = "@/home/pi/apps/clicks/face_"+str(f)+".jpg"
                     #os.system('curl -X POST https://s3.amazonaws.com/standbye -F "key='+key+'" -F "file='+file+'" -F "Content-Type=image/jpeg"')
                 os.system("omxplayer -o local /home/pi/apps/Christmas-doorbell-melody.mp3")
-                #- start of idle loop
-                for i in range(0, 240):
-                    ret, img = cam.read()
-                    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                    faces = face_cascade.detectMultiScale(gray, 1.6, 6)
-                    for (x,y,w,h) in faces:
-                        cv2.rectangle(gray,(x,y),(x+w,y+h),(255,255,255), 2)
-                    cv2.imshow("Main Door", gray)
-                    cv2.waitKey(250)
-                #- end of idle loop
                 os.system("rm /home/pi/apps/clicks/*.jpg")
+                working = 240
                 facenum = 0
             else:
                 facenum = 0
